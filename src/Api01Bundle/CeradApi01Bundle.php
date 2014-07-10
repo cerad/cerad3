2a9750f2f1bf82365f609dbed2fd7525ac3a21a6
ahundiak <ahundiak@spike.mshome.net>
2014-07-10 12:09:12 -0500

    Initial Commit Removed AcmeDemo Added CeradApi01Bundle with psr-4 loading Simple GET /projects/xxx/games/yyy with test ProjectGameBundle Skeleton ProjectCoreBundle Added Game entity Added Docs Action Added Twig namespace pointing to Action directory

--- src/Api01Bundle/CeradApi01Bundle.php
+++ src/Api01Bundle/CeradApi01Bundle.php
@@ -3,7 +3,16 @@
 namespace Cerad\Bundle\Api01Bundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+use Cerad\Bundle\Api01Bundle\DependencyInjection\Compiler\Pass;
 
 class CeradApi01Bundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new Pass());
+    }
 }
--- src/Api01Bundle/DependencyInjection/CeradApi01Extension.php
+++ src/Api01Bundle/DependencyInjection/CeradApi01Extension.php
@@ -12,6 +12,10 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
+/* ==========================================
+ * By default, this extension does not know anything about the bundle
+ * The configs are empty
+ */
 class CeradApi01Extension extends Extension
 {
     /**
@@ -21,8 +25,19 @@ class CeradApi01Extension extends Extension
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
-
+        if ($config);
+        
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
+        
+        // Need to test throughly n production
+        // C:\home\ahundiak\zayso2016\cerad3\src\Api01Bundle
+        $bundleDir = dirname(dirname(__FILE__));
+        $container->setParameter('cerad_api01__bundle_dir',$bundleDir);
+        
+        
+        // This does not work, need a pass
+        // $twigFilesystemLoaderDefinition = $container->getDefinition('twig.loader.filesystem');
+        
     }
 }
