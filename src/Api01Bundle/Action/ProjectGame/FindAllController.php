3adbbc7b84b76e43ea6a2e38e6c4ab447256d7af
ahundiak <ahundiak@spike.IGSLAN.com>
2014-07-18 13:37:55 -0500

    Changed Find to FindOne Removed Controller Directory FindAll ProjectGames ProjectLevels Added teams to json

--- src/Api01Bundle/Action/ProjectGame/FindAllController.php
+++ src/Api01Bundle/Action/ProjectGame/FindAllController.php
@@ -2,43 +2,11 @@
 namespace Cerad\Bundle\Api01Bundle\Action\ProjectGame;
 
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+//  Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
-class FindAllController
+class FindAllController extends FindBaseController
 {
-    protected $gameRepo;
-    
-    public function __construct($gameRepo)
-    {
-        $this->gameRepo = $gameRepo;
-    }
-    protected function processParam(&$params,$query,$key)
-    {
-        // Make sure have something
-        if (!isset($query[$key])) return;
-        
-        $params[$key] = explode(',',$query[$key]);
-    }
-    protected function serializeGame($game)
-    {
-        // Serializing is painful
-        $item = array(
-            'projectKey' => $game->getProjectKey(),
-            'num'        => $game->getNum(),
-            'dtBeg'      => $game->getDtBeg()->format('Y-m-d H:i:s'),
-            'dtEnd'      => $game->getDtEnd()->format('Y-m-d H:i:s'),
-            'venueName'  => $game->getVenueName(),
-            'fieldName'  => $game->getFieldName(),
-            
-            'levelKey'   => $game->getLevelKey(),
-            'groupType'  => $game->getGroupType(),
-            'groupName'  => $game->getGroupName(),
-            'status'     => $game->getStatus(),
-            'teams'      => array(),
-        );
-        return $item;
-    }
     public function __invoke(Request $request, $projectId)
     {
         $query = $request->query->all();
